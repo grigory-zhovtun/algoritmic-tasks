f706a4b4dcecccb438c4641c0667119bf74e3e47
grigory-zhovtun <grigory.zhovtun@gmail.com>
2022-04-26 21:21:28 +0300

    task_15

--- task_15.js
+++ task_15.js
@@ -1,1 +1,20 @@
 'use strict';
+
+function sumAll(arr) {
+    let sum = 0;
+    let [a, b] = arr.sort((a, b) => a - b);
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+
+    return sum;
+  }
+  
+  console.log(sumAll([1, 4]));
+
+  /**
+  We'll pass you an array of two numbers. 
+  Return the sum of those two numbers plus the sum of all 
+  the numbers between them. 
+  The lowest number will not always come first.
+  */
